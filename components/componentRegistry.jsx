2155b2be790c7533cc86ad3a284e76ece4320be7
Blinkflo Builder <builder@blinkflo.dev>
2026-02-28 05:39:57 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,28 @@
+
+import SectionHeader from "./sectionHeader";
+import ContactSupportOptions from "./contactSupportOptions";
+import ContactOurOffices from "./ContactOurOffices";
+import AboutHeroSection from "./aboutHeroSection";
+import AboutMetric from "./aboutMetric";
+import AboutOurStory from "./aboutOurStory";
+import AboutOurTeam from "./aboutOurTeam";
+import AboutOurValues from "./aboutOurValues";
+import TransformSection from "./TransformSection";
+import OurServices from "./ourServices";
+import Header from "./header";
+import Footer from "./footer";
+
+export const componentRegistry = {
+  "section-header": SectionHeader,
+  "contact-support-options": ContactSupportOptions,
+  "contact-our-offices": ContactOurOffices,
+  "about-hero": AboutHeroSection,
+  "about-metric": AboutMetric,
+  "about-our-story": AboutOurStory,
+  "about-our-team": AboutOurTeam,
+  "about-our-values": AboutOurValues,
+  "transform-section": TransformSection,
+  "our-services": OurServices,
+  "header-sec": Header,
+  "footer-section": Footer
+};
